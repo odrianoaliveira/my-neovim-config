b1c5cbd246cd0db69c5e933cd16dfbf352c5de21
Adriano de Oliveira <5173823+odrianoaliveira@users.noreply.github.com>
2025-07-06 17:53:45 +0200

    Install Go linters and formtatters

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -7,7 +7,12 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.completion.spell,
-				--require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+				null_ls.builtins.diagnostics.ktlint,
+				null_ls.builtins.code_actions.gomodifytags,
+				null_ls.builtins.code_actions.impl,
+				null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.formatting.gofmt,
+				null_ls.builtins.formatting.goimports,
 			},
 		})
 
