cafee3bc0f50652f80a3a35bb4b7c2a1398f92f7
Adriano de Oliveira <5173823+odrianoaliveira@users.noreply.github.com>
2025-07-06 18:49:59 +0200

    Install prettier

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -7,12 +7,17 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.completion.spell,
+
 				null_ls.builtins.diagnostics.ktlint,
+
 				null_ls.builtins.code_actions.gomodifytags,
 				null_ls.builtins.code_actions.impl,
 				null_ls.builtins.diagnostics.golangci_lint,
 				null_ls.builtins.formatting.gofmt,
 				null_ls.builtins.formatting.goimports,
+
+				--null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.formatting.prettier,
 			},
 		})
 
